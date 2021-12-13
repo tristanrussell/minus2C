07f2f28b2d8beb5cd2f62932b27d1b3bcbf1a43c
Tristan Russell <39279889+tristanrussell@users.noreply.github.com>
2021-12-13 22:10:45 +0000

    Improve readability of structures.

--- structs.h
+++ structs.h
@@ -16,14 +16,14 @@ typedef struct ar AR;
 typedef struct mc MC;
 
 typedef struct frame {
-    BINDING*    bindings;
-    FRAME*      next;
+    BINDING*  bindings;
+    FRAME*    next;
 } FRAME;
 
 typedef struct binding {
-    TOKEN*      name;
-    VALUE*      val;
-    BINDING*    next;
+    TOKEN*    name;
+    VALUE*    val;
+    BINDING*  next;
 } BINDING;
 
 typedef struct closure
@@ -36,46 +36,46 @@ typedef struct closure
 
 typedef struct value
 {
-    int          type;
+    int  type;
     union {
-        int integer;
-        int boolean;
-        char* string;
-        CLOSURE* closure;
-        VALUE *ret;
+        int       integer;
+        int       boolean;
+        char*     string;
+        CLOSURE*  closure;
+        VALUE*    ret;
     } v;
 } VALUE;
 
 typedef struct node {
-    int          type;
-    struct node *left;
-    struct node *right;
+    int    type;
+    NODE*  left;
+    NODE*  right;
 } NODE;
 
 typedef struct token {
-    int           type;
-    char          *lexeme;
-    int           value;
-    TOKEN  *next;
+    int     type;
+    char*   lexeme;
+    int     value;
+    TOKEN*  next;
 } TOKEN;
 
 typedef struct proc {
-    TOKEN* name;
-    AR* ar;
+    TOKEN*  name;
+    AR*     ar;
 } PROC;
 
 typedef struct endproc {
-    TAC* start;
+    TAC*  start;
 } ENDPROC;
 
 typedef struct block {
-    int nvars;
-    TOKEN **vars;
+    int      nvars;
+    TOKEN**  vars;
 } BLOCK;
 
 typedef struct call {
-    TOKEN* name;
-    AR* ar;
+    TOKEN*  name;
+    AR*     ar;
 } CALL;
 
 typedef struct line {
@@ -85,11 +85,11 @@ typedef struct line {
 } LINE;
 
 typedef struct taclabel {
-    TOKEN* name;
+    TOKEN*  name;
 } TACLABEL;
 
 typedef struct tacif {
-    TAC*    cond;
+    TAC*       cond;
     TACLABEL*  label;
 } TACIF;
 
@@ -98,40 +98,40 @@ typedef struct tacgoto {
 } TACGOTO;
 
 typedef struct tac {
-    int     op;
+    int  op;
     union {
-        PROC proc;
-        ENDPROC endproc;
-        BLOCK block;
-        CALL call;
-        LINE line;
-        TACLABEL taclabel;
-        TACIF tacif;
-        TACGOTO tacgoto;
+        PROC      proc;
+        ENDPROC   endproc;
+        BLOCK     block;
+        CALL      call;
+        LINE      line;
+        TACLABEL  taclabel;
+        TACIF     tacif;
+        TACGOTO   tacgoto;
     } args;
-    TAC*    next;
+    TAC*  next;
 } TAC;
 
 typedef struct bb {
-    TAC*    leader;
-    BB*     next;
+    TAC*  leader;
+    BB*   next;
 } BB;
 
 typedef struct ar {
-    AR *fp; // save caller ’s AR
-    char pc; // save caller ’s PC
-    AR *sl; // this function ’s static link
-    TOKEN **param; // param0 , ... paramm
-    int arity;
-    TOKEN **local; // local0 , ... localn
-    int localCount;
-    TOKEN **tmp; //tmp0 , ... tmpn
-    int tmpCount;
+    AR*      fp;
+    char     pc;
+    AR*      sl;
+    TOKEN**  param;
+    int      arity;
+    TOKEN**  local;
+    int      localCount;
+    TOKEN**  tmp;
+    int      tmpCount;
 } AR;
 
 typedef struct mc {
-    char* insn;
-    MC* next;
+    char*  insn;
+    MC*    next;
 } MC;
 
 #endif //__STRUCTS_H
